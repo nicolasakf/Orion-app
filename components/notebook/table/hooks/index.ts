d31cd013d9f2bef62c63afc7432cda032cb22e80
nicolasakf <nicolasakfonteyne@gmail.com>
2026-05-19 23:37:24 -0400

    refactor(notebook): remove DataTable output viewer
    Delete the unused notebook table UI and related hooks now that table
    display settings were removed from persisted user settings.

--- components/notebook/table/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { useDisplaySettings } from "./use-display-settings";
-export { useTableFilters } from "./use-table-filters";
-export { useTableSelection } from "./use-table-selection";
-export { useTableData } from "./use-table-data";
-export { useTableKeyboard } from "./use-table-keyboard";
-export { useTableViews } from "./use-table-views";
-export { useTableExport } from "./use-table-export";
-export { useColumnStats } from "./use-column-stats";
